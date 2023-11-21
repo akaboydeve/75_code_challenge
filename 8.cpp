60de5d0b1c1bdec02323b301cdfc419efa0ada59
Akash Sardar <73843791+akaboydeve@users.noreply.github.com>
2023-11-21 19:27:48 +0530

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using std::max;
+using std::min;
+using std::vector;
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+        int ans = nums[0];
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            if (nums[left] <= nums[mid])
+            {
+                if (target < nums[left] || target > nums[mid])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            else
+            {
+                if (target < nums[mid] || target > nums[right])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
