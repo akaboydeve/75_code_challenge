49c42c460555d55d62987f76394103f78c548b54
Akash Sardar <73843791+akaboydeve@users.noreply.github.com>
2023-11-26 20:06:54 +0530

    122

--- 13.cpp
+++ 13.cpp
@@ -2,18 +2,21 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <bits/stdc++.h>
 
+using std::max;
+using std::min;
 using std::vector;
+using namespace std;
 
 class Solution
 {
 public:
     vector<int> countBits(int n)
     {
-        vector<int> ans(n + 1);
+        vector<int> ans;
         ans[0] = 0;
         int offset = 1;
-
         for (int i = 1; i <= n; i++)
         {
             if (offset * 2 == i)
