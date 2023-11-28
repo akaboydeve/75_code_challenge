0b8939b787edba24f2856160c43ca6f1030e51d7
Akash Sardar <73843791+akaboydeve@users.noreply.github.com>
2023-11-28 21:17:07 +0530

    Create 15.cpp

---
+++ 15.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <bits/stdc++.h>
+
+using std::max;
+using std::min;
+using std::vector;
+using namespace std;
+
+class Solution
+{
+public:
+    uint32_t reverseBits(uint32_t n)
+    {
+        uint32_t result = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            result = result << 1;
+            if ((n % 2) & 1)
+            {
+                result = result | 1;
+            }
+            n = n >> 1;
+        }
+        return result;
+    }
+};
