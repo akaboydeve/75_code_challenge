962f04222dfb9592fde66cc66d4fe5e4d5492510
Akash Sardar <73843791+akaboydeve@users.noreply.github.com>
2023-12-03 22:21:25 +0530

    Create 20.cpp

---
+++ 20.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <bits/stdc++.h>
+#include <algorithm>
+
+using std::max;
+using std::min;
+using std::vector;
+using namespace std;
+
+class Solution
+{
+public:
+    bool wordBreak(string s, vector<string> &wordDict)
+    {
+        int n = s.length();
+        vector<bool> dp(n + 1, false);
+        dp[n] = true;
+
+        for (int i = n - 1; i >= 0; --i)
+        {
+            for (const string &w : wordDict)
+            {
+                if ((i + w.length()) <= n && s.substr(i, w.length()) == w)
+                {
+                    dp[i] = dp[i + w.length()];
+                }
+                if (dp[i])
+                {
+                    break;
+                }
+            }
+        }
+
+        return dp[0];
+    }
+};
