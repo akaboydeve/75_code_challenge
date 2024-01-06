f07d1f829c9675a63e979974c416c0b4672af7f2
Akash Sardar <73843791+akaboydeve@users.noreply.github.com>
2024-01-06 20:23:24 +0530

    Create 49.cpp

---
+++ 49.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <unordered_set>
+
+using std::max;
+using std::min;
+using std::vector;
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        map<map<char, int>, vector<string>> mapS;
+
+        for (const string &str : strs)
+        {
+            map<char, int> tempMap;
+
+            for (char c : str)
+            {
+                tempMap[c]++;
+            }
+
+            mapS[tempMap].push_back(str);
+        }
+
+        vector<vector<string>> anagrams;
+        for (const auto &p : mapS)
+        {
+            anagrams.push_back(p.second);
+        }
+
+        return anagrams;
+    }
+};
+
+class Solution
+{
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        unordered_map<string, vector<string>> mapS;
+        for (auto s : strs)
+        {
+            string word = s;
+            sort(word.begin(), word.end());
+            mapS[word].push_back(s);
+        }
+        vector<vector<string>> res;
+        for (auto s : mapS)
+        {
+            res.push_back(s.second);
+        }
+        return res;
+    }
+};
