a298aef5bf251ca7f0a1a0b6da0bdb77b34b085b
Akash Sardar <73843791+akaboydeve@users.noreply.github.com>
2024-01-12 20:51:04 +0530

    Create 54.cpp

---
+++ 54.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <unordered_set>
+
+using std::max;
+using std::min;
+using std::vector;
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (!root)
+        {
+            return 0;
+        }
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
