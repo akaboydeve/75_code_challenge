fa26dc7f6afc23c4676bed9d3ae31b6bb943359b
Akash Sardar <73843791+akaboydeve@users.noreply.github.com>
2024-01-16 20:33:59 +0530

    Create 56.cpp

---
+++ 56.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <unordered_set>
+
+using std::max;
+using std::min;
+using std::vector;
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (!root)
+        {
+            return nullptr;
+        }
+
+        TreeNode *temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+    }
+};
